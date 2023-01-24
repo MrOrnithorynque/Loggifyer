b2f594bc339e74ffb53586c8e3f686d1f553aa8b
Louis <louis.obeniche-rousse@epitech.eu>
2023-01-24 18:47:55 +0100

    ✅ : loggifyer ostream test clang

--- tests/testMain.cpp
+++ tests/testMain.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    PTP_LOG_INFO("This is a Info message");
+    PTP_LOG_INFO("This is a Info message" << " with a stream" << " and a number: " << 42);
     PTP_LOG_MESSAGE("This is just a simple message.");
     PTP_LOG_OK("This is a Ok message to tell you that everything working properly.");
     PTP_LOG_WARNING("Be careful! you are looking good today. This was a Warning message.");
