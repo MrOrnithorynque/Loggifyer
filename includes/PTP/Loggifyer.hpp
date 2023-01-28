981bb67dc48b70065d81f53e99083577f10cb4c7
Louis <louis.obeniche-rousse@epitech.eu>
2023-01-28 20:27:55 +0100

    doxygen

--- includes/PTP/Loggifyer.hpp
+++ includes/PTP/Loggifyer.hpp
@@ -4,7 +4,7 @@
 
 namespace ptplog = ptp::log;
 
-/*
+/**
  * The 'void()è at the end of some of the macro are here to force to write ';' at the end of the PT_LOG macro.
  */
 
@@ -61,91 +61,51 @@ namespace ptplog = ptp::log;
  * @note  {0, 0, 0, 0},
  * @note  {0, 0, 0, 0}}
  */
-#define PTP_LOG_COMPLEX_FORMATTING(bEnableComplexFormatting)    \
-    if (bEnableComplexFormatting == true)                       \
-        ptplog::Logger::getInstance().enableComplexFormatting();\
-    else                                                        \
+#define PTP_LOG_COMPLEX_FORMATTING(bEnableComplexFormatting)            \
+    if (bEnableComplexFormatting == true)                               \
+        ptplog::Logger::getInstance().enableComplexFormatting();        \
+    else                                                                \
         ptplog::Logger::getInstance().disableComplexFormatting(); void()
 
-#define PTP_LOG_ENABLE_COMPLEX_FORMATTING   \
-    PTP_LOG_COMPLEX_FORMATTING(true)
+/**
+ * @brief Macro to enable complex formatting on logs.
+ */
+#define PTP_LOG_ENABLE_COMPLEX_FORMATTING() PTP_LOG_COMPLEX_FORMATTING(true)
 
+/**
+ * @brief Macro to disable complex formatting on logs.
+ */
 #define PTP_LOG_SET_OUTPUT(output) ptplog::Logger::getInstance().setOutput(output)
 
+/**
+ * @brief Macro to set the output file.
+ */
 #define PTP_LOG_SET_OUTPUT_FILE(fileName) { \
-    std::ofstream file(fileName);       \
-    PTP_SET_OUTPUT(file);               \
-} void() \
+    std::ofstream file(fileName);           \
+    PTP_SET_OUTPUT(file);                   \
+} void()
 
+/**
+ * @brief Macro to set the workspace dir.
+ * @note Remove the workspace dir from the filepath so the log file is more readable.
+ */
 #define PTP_LOG_SET_WORKSPACE_DIR(workspaceDir) ptplog::Logger::getInstance().setWorkspaceDir(workspaceDir)
 
+/**
+ * @brief Macro to set the output of the logs.
+ */
 #define PTP_LOG_DISPLAY_FILEPATH(bDisplayFilepath) ptplog::Logger::getInstance().displayFilepath(bDisplayFilepath)
 
 #if defined _WIN32
 
+    /**
+     * @brief Macro print log message.
+     * @param LogLevel log level.
+     * @param message log message.
+     */
     #define PTP_LOG(LogLevel, message, ...) { std::ostringstream stream; stream << message;                 \
         if (PTP_IS_LOG_ENABLE() && LogLevel >= PTP_GET_LOG_LEVEL())                                         \
             ptplog::Logger::getInstance().log(LogLevel, __FILE__, __LINE__, stream, __VA_ARGS__); } void()  \
-
-    /**
-     * @brief Macro print message log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_MESSAGE("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_MESSAGE("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_MESSAGE(message, ...) PTP_LOG(ptplog::LogLevel::Message, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print info log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_INFO("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_INFO("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_INFO(message, ...) PTP_LOG(ptplog::LogLevel::Info, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print ok log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_OK("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_OK("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_OK(message, ...) PTP_LOG(ptplog::LogLevel::Ok, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print warning log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_WARNING("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_WARNING("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_WARNING(message, ...) PTP_LOG(ptplog::LogLevel::Warning, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print error log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_ERROR("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_ERROR("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_ERROR(message, ...) PTP_LOG(ptplog::LogLevel::Error, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print error log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_FATAL("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_FATAL("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_FATAL(message, ...) PTP_LOG(ptplog::LogLevel::Fatal, message, __VA_ARGS__)
 
 #elif defined __linux__ || __APPLE__
 
@@ -154,68 +114,68 @@ namespace ptplog = ptp::log;
      * @param LogLevel log level.
      * @param message log message.
      */
-    #define PTP_LOG(LogLevel, message, ...) { std::ostringstream stream; stream << message;                         \
+    #define PTP_LOG(LogLevel, message, ...) { std::ostringstream stream; stream << message;                             \
         if (PTP_IS_LOG_ENABLE() && LogLevel >= PTP_GET_LOG_LEVEL())                                                     \
-            ptplog::Logger::getInstance().log(LogLevel, __FILE__, __LINE__, stream __VA_OPT__(,) __VA_ARGS__); } void()\
-
-    /**
-     * @brief Macro print message log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_MESSAGE("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_MESSAGE("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_MESSAGE(message, ...) PTP_LOG(ptplog::LogLevel::Message, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print info log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_INFO("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_INFO("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_INFO(message, ...) PTP_LOG(ptplog::LogLevel::Info, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print ok log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_OK("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_OK("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_OK(message, ...) PTP_LOG(ptplog::LogLevel::Ok, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print warning log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_WARNING("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_WARNING("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_WARNING(message, ...) PTP_LOG(ptplog::LogLevel::Warning, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print error log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_ERROR("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_ERROR("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_ERROR(message, ...) PTP_LOG(ptplog::LogLevel::Error, message, __VA_ARGS__)
-
-    /**
-     * @brief Macro print fatal log message.
-     *
-     * @note It used printf or cpp stream format style.
-     * @note If you want to use printf format style : PTP_LOG_FATAL("message %d", 1);
-     * @note If you want to use cpp stream format style : PTP_LOG_FATAL("message " << 1);
-     * @note But not both.
-     */
-    #define PTP_LOG_FATAL(message, ...) PTP_LOG(ptplog::LogLevel::Fatal, message, __VA_ARGS__)
+            ptplog::Logger::getInstance().log(LogLevel, __FILE__, __LINE__, stream __VA_OPT__(,) __VA_ARGS__); } void() \
 
 #endif
+
+/**
+ * @brief Macro print message log message.
+ *
+ * @note It used printf or cpp stream format style.
+ * @note If you want to use printf format style : PTP_LOG_MESSAGE("message %d", 1);
+ * @note If you want to use cpp stream format style : PTP_LOG_MESSAGE("message " << 1);
+ * @note But not both.
+ */
+#define PTP_LOG_MESSAGE(message, ...) PTP_LOG(ptplog::LogLevel::Message, message, __VA_ARGS__)
+
+/**
+ * @brief Macro print info log message.
+ *
+ * @note It used printf or cpp stream format style.
+ * @note If you want to use printf format style : PTP_LOG_INFO("message %d", 1);
+ * @note If you want to use cpp stream format style : PTP_LOG_INFO("message " << 1);
+ * @note But not both.
+ */
+#define PTP_LOG_INFO(message, ...) PTP_LOG(ptplog::LogLevel::Info, message, __VA_ARGS__)
+
+/**
+ * @brief Macro print ok log message.
+ *
+ * @note It used printf or cpp stream format style.
+ * @note If you want to use printf format style : PTP_LOG_OK("message %d", 1);
+ * @note If you want to use cpp stream format style : PTP_LOG_OK("message " << 1);
+ * @note But not both.
+ */
+#define PTP_LOG_OK(message, ...) PTP_LOG(ptplog::LogLevel::Ok, message, __VA_ARGS__)
+
+/**
+ * @brief Macro print warning log message.
+ *
+ * @note It used printf or cpp stream format style.
+ * @note If you want to use printf format style : PTP_LOG_WARNING("message %d", 1);
+ * @note If you want to use cpp stream format style : PTP_LOG_WARNING("message " << 1);
+ * @note But not both.
+ */
+#define PTP_LOG_WARNING(message, ...) PTP_LOG(ptplog::LogLevel::Warning, message, __VA_ARGS__)
+
+/**
+ * @brief Macro print error log message.
+ *
+ * @note It used printf or cpp stream format style.
+ * @note If you want to use printf format style : PTP_LOG_ERROR("message %d", 1);
+ * @note If you want to use cpp stream format style : PTP_LOG_ERROR("message " << 1);
+ * @note But not both.
+ */
+#define PTP_LOG_ERROR(message, ...) PTP_LOG(ptplog::LogLevel::Error, message, __VA_ARGS__)
+
+/**
+ * @brief Macro print error log message.
+ *
+ * @note It used printf or cpp stream format style.
+ * @note If you want to use printf format style : PTP_LOG_FATAL("message %d", 1);
+ * @note If you want to use cpp stream format style : PTP_LOG_FATAL("message " << 1);
+ * @note But not both.
+ */
+#define PTP_LOG_FATAL(message, ...) PTP_LOG(ptplog::LogLevel::Fatal, message, __VA_ARGS__)
